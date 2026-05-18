a15dd51ecbfcd897fa42fd051b9e02eee64259c9
EdinburghAxy <wonginseong@gmail.com>
2026-05-18 10:34:24 +0100

    cmmuit

--- sketch.js
+++ sketch.js
@@ -1,8 +1,27 @@
 function setup() {
-  strokeWeight(0);
   createCanvas(600, 400);
+  fill(255, 0, 0);
 }
 
 function draw() {
-  circle(300, 200, 100);
+  if (keyIsDown(65) || keyIsDown(97)) { // 'A' or 'a'
+    x -= 5;
+  }
+
+  if (keyIsDown(68) || keyIsDown(100)) { // 'D' or 'd'
+    x += 5;
+  }
+
+  if (keyIsDown(87) || keyIsDown(119)) { // 'W' or 'w'
+    y -= 5;
+  }
+
+  if (keyIsDown(83) || keyIsDown(115)) { // 'S' or 's'
+    y += 5;
+  }
+
+  clear();
+  ellipse(x, y, 50, 50);
+  describe(`50-by-50 red ellipse moves left, right, up, and
+    down with WASD presses.`);
 }
