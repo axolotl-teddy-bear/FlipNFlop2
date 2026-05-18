e9beec6d0da7713c38d478696f3dd2127934e9eb
EdinburghAxy <wonginseong@gmail.com>
2026-05-18 14:29:18 +0100

    ff

--- sketch.js
+++ sketch.js
@@ -1,34 +1,70 @@
 let x = 100;
 let y = 100;
+let pressStartTime = 0;
+let currentState = "rest"; //keep track of obj state
 
-function setup() {
-  createCanvas(600, 400);
-  fill(100, 0, 0);
+function preload() {
+  img1 = loadImage('/assets/images/fish1.png');
+  img2 = loadImage('/assets/images/fish2.png');
+  img3 = loadImage('/assets/images/fish3.png');
 }
 
-function draw() {
+function setup() { // only runs ONCE
+  createCanvas(windowWidth, windowHeight);
+}
+
+function mousePressed() {
+  pressStartTime = millis();
+  currentShape = "rest";
+}
+
+function draw() { //runs REPEATEDLY
+
+  function mouseReleased() {
+    currentShape = "rest";
+  }
+
+  if (!mouseIsPressed) {
+    if (keyIsDown(65) || keyIsDown(97)) { // 'A' or 'a'
+    x -= 5;
+    }
+
+    if (keyIsDown(68) || keyIsDown(100)) { // 'D' or 'd'
+    x += 5;
+    }
+
+   if (keyIsDown(87) || keyIsDown(119)) { // 'W' or 'w'
+    y -= 5;
+    }
+
+   if (keyIsDown(83) || keyIsDown(115)) { // 'S' or 's'
+    y += 5;
+   }
+  }
+  
+  background(255, 204, 0); //gets rid of repeating shapes
 
   if (mouseIsPressed) {
-    square(300, 200, 50);
-  } else if (mouseIsPressed === false) {
-      if (keyIsDown(65) || keyIsDown(97)) { // 'A' or 'a'
-        x -= 5;
-      }
+    let holdDuration = millis() - pressStartTime;
 
-      if (keyIsDown(68) || keyIsDown(100)) { // 'D' or 'd'
-        x += 5;
-      }
+    if (holdDuration < 1000) { //if mouse being hold less then a sec
+      currentState = "ready"
+    } else { //more then a sec
+      currentState = "tense"
+    }
+  } else { //mouse button not being held
+    currentState = "rest"
+}
 
-      if (keyIsDown(87) || keyIsDown(119)) { // 'W' or 'w'
-        y -= 5;
-      }
+if (currentState === "rest") {
+  Image(img1, x, y)
+} else if (currentState === "tense") {
+  shake = random(-2, 2);
+  ellipse(x, y, 50 + shake, 50 + shake);
+} else if (currentState === "ready") {
+  square(x, y, 50);
+}
 
-      if (keyIsDown(83) || keyIsDown(115)) { // 'S' or 's'
-        y += 5;
-      }
 
-    clear();
-    ellipse(x, y, 50, 50);
-  }
 
 }
