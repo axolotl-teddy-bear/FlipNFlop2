761ba4978c9e528de80ea72507030cfeba06f46b
axolotl-teddy-bear <wonginseong@gmail.com>
2026-05-21 15:39:31 +0100

    edit

--- sketch.js
+++ sketch.js
@@ -35,7 +35,6 @@ function preload() {
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  img4.resize(1500, 700);
   imageMode(CENTER); //sets origin at center of image, not corner
   img1.resize(320, 250);
   img2.resize(320, 250);
