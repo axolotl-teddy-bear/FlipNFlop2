f820f6c1d5683c5e204936e4e8754578c5a573c7
axolotl-teddy-bear <wonginseong@gmail.com>
2026-05-21 15:40:15 +0100

    edit

--- sketch.js
+++ sketch.js
@@ -39,6 +39,7 @@ function setup() {
   img1.resize(320, 250);
   img2.resize(320, 250);
   img3.resize(320, 250);
+  img4.resize(500, 300);
 }
 
 function mousePressed() { //when mouse is pressed, the time stats recording. 
@@ -214,6 +215,7 @@ function updateSlide() {
 
 function draw() {
   background(255, 204, 0);
+  //image(img4, 0, 0);
 
   // Update state based on mouse hold (only when not jumping)
   if (!isJumping && !isSliding) {
